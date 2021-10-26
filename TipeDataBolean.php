1c771cf48a622b4dd5577c184f82599ed58f6116
Miftahul Fajri Sidqi <miftahulfajrisidqi529@gmail.com>
2021-10-26 19:50:43 +0700

    update

---
+++ TipeDataBolean.php
@@ -0,0 +1,13 @@
+<?php 
+echo "Benar : ";
+var_dump(true);
+
+echo "salah : " ;
+var_dump(false);
+/*
+- Tipe data boolean adalah tipe data paling sederhana di PHP
+- Tipe data boolean adalah tipe data dengan nilai kebenaran (benar atau salah)
+- Nilai benar direpresentasikan dengan true (case insensitive)
+- Nilai salah direpresentasikan dengan false (case insensitive)
+*/
+?>
